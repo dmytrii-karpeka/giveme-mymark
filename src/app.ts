58d4a0981375dea5f1e28361eac877fcd8ca2f13
dmytrii-karpeka <dmitriy.karpeka@gmail.com>
2023-02-08 19:47:21 +0200

    working stable version of bot

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,53 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 import { Context, Markup, Telegraf } from 'telegraf';
 import { Update } from 'typegram';
-console.log(process.env);
+
+const Sheets = require("node-sheets").default;
+
+const nameH: string = "Найближчий урок";
+const markH: string = "Загальний баланс логіків";
+
+async function getTable() {
+    const gs = new Sheets(process.env.SHEET_ID)
+    await gs.authorizeApiKey(process.env.GOOGLE_SHEET_KEY);
+    const table = await gs.tables("Викладач!A:G");
+    return table;
+}
+
+function getMarkByName(name: string, table: any) {
+    const filteredRows = table.rows.filter((row: any) => {
+        if (typeof row !== 'undefined') {
+            if (typeof row[nameH] !== "undefined") {
+                return typeof row[nameH].stringValue !== "undefined" || row[nameH].stringValue !== 'Учень';
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    })
+    const row = filteredRows.find((student: any) => {
+        if (student[nameH]) {
+            return student[nameH].stringValue.toLowerCase() === name.toLowerCase();
+        }
+    });
+    if (row) {
+        return `Кількість логіків ${row[nameH].stringValue} дорівнює: ${row[markH].stringValue} 🧩`;
+    } else {
+        return `Перевірте вказане ім'я та прізвище на написання. Якщо і надалі бачите це повідомлення, то зверніться до викладача.`
+    }
+}
+
+
 const bot: Telegraf<Context<Update>> = new Telegraf(process.env.BOT_TOKEN as string);
 
 bot.start((ctx) => {
     ctx.reply('Привіт ' + ctx.from.first_name + '!');
     ctx.reply('Цей невеликий бот допоможе тобі у тому, щоб дізнатись про свою оцінку і не турбувати викладача.');
     ctx.reply("Для того, щоб дізнатись оцінку введи своє Прізвище Ім'я\nНаприклад:");
-    ctx.reply("Карпека Дмитрій");
+    ctx.reply("Дмитро Карпенко")
 });
 
 bot.help((ctx) => {
@@ -18,8 +58,13 @@ bot.help((ctx) => {
 
 
 bot.on('text', async (ctx) => {
+    await getTable().then(async (result: any) => {
+        ctx.reply(
+            getMarkByName(ctx.message.text, result)
+        );
+    });
     ctx.reply(
-        "Дякую за використання цього бота!"
+        "Дякую за використання цього бота! Зичу успіхів у навчанні!"
     );
 });
 
